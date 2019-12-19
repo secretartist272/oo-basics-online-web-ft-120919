413141f5b085301dbdc778cbca3480e345f3f794
Jacqueline Freeland <secretartist272@gmail.com>
2019-12-19 20:38:41 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,10 +1,10 @@
 class Shoe 
-  def inititalize(brand)
-    @brand = brand
-  end
   
   attr_reader :brand
   attr_accessor :color, :size, :material, :condition
+  def inititalize(brand)
+    @brand = brand
+  end
   
   def brand
     @brand=
