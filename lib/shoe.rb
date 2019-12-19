6534c9dab598f48c98f2ee51ebc7bce14f97041d
Jacqueline Freeland <secretartist272@gmail.com>
2019-12-19 20:52:10 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,15 +1,22 @@
-class Shoe 
+require 'pry'
+
+class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
-  
-  def inititalize(brand)
+
+  BRANDS = []
+
+  def initialize(brand)
     @brand = brand
+    if BRANDS.include?(brand)
+    else
+      BRANDS << brand
+    end
   end
-  
+
   def cobble
+    self.condition = "new"
     puts "Your shoe is as good as new!"
-    @condition= "new"
   end
 
 end
-
