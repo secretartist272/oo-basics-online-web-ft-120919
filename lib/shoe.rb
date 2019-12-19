7a337ac719f7ba85cac430b4f8d6e260afb864e7
Jacqueline Freeland <secretartist272@gmail.com>
2019-12-19 20:30:01 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -3,8 +3,13 @@ class Shoe
     @brand = brand
   end
   
-  def brand
-    @brand
+   attr_reader :brand
+  attr_accessor :color, :size, :material, :condition
+
+  def cobble()
+    self.condition = "new"
+    puts "Your shoe is as good as new!"
   end
+
 end
 
